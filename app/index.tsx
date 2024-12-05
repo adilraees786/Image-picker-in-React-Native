673f94539d6c6949a03425f4d648559ecf80bba5
adilraees786 <adilraees911@gmail.com>
2024-12-05 20:41:03 +0500

    add camera and image icon

--- app/index.tsx
+++ app/index.tsx
@@ -2,6 +2,7 @@
 import { ThemedView } from "@/components/ThemedView";
 import { useState } from "react";
 import { FlatList, Image } from "react-native";
+import Entypo from '@expo/vector-icons/Entypo';
 
 const imageArray = [
     "https://images.unsplash.com/photo-1501786223405-6d024d7c3b8d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bW91bnRhaW58ZW58MHx8MHx8fDA%3D",
@@ -20,20 +21,38 @@ export default function ImageGallery() {
     return (
         <ThemedView style={{ flex: 1 }} >
             {/* <ThemedText type="title"> Image Gallery</ThemedText> */}
-        <FlatList
-        data={imageArray}
-        keyExtractor={(data) => data}
-        renderItem={({item, index}) => {
-            return (
-         <Image
-         source={{uri: item}}
-         style={{height: 230, marginVertical: 10,}}
-         
-    
-         />
-            );
-        }}
-        />
+            <ThemedView style={{ padding: 20, flexDirection: "row" }}>
+                <Entypo style={{
+                    backgroundColor: "#ccc",
+                    padding: 12,
+                    borderRadius: 125,
+
+                }}
+                    name="images" size={24} color="white" />
+                <Entypo style={{
+                    backgroundColor: "#ccc",
+                    padding: 12,
+                    borderRadius: 125,
+
+                }}
+                    name="camera" size={24} color="white" />
+            </ThemedView>
+
+
+            <FlatList
+                data={imageArray}
+                keyExtractor={(data) => data}
+                renderItem={({ item, index }) => {
+                    return (
+                        <Image
+                            source={{ uri: item }}
+                            style={{ height: 230, marginVertical: 10, }}
+
+
+                        />
+                    );
+                }}
+            />
         </ThemedView>
 
     );
