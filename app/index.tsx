22103cfd53863f7df1c17a35b83e049e3d31d1fa
adilraees786 <adilraees911@gmail.com>
2024-12-05 12:51:39 +0500

    add index file

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+
+
+
+export default function ImageGallery(){
+    return(
+        <ThemedView>
+            <ThemedText type="title"> Image Gallery</ThemedText>
+        </ThemedView>
+
+    )
+}
